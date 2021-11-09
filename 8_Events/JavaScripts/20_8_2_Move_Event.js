1a6635bf49e5cbee7512c4b84ecc3b18c78a8516
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-09 23:50:38 +0400

    JavaScript > Event > Mouse Event > add

--- 8_Events/JavaScripts/20_8_2_Move_Event.js
+++ 8_Events/JavaScripts/20_8_2_Move_Event.js
@@ -3,12 +3,31 @@ const btn=document.querySelector('#btnDeleteAll');
 const ul = document.querySelector('#task-list');
 
 
-    // click => 1 click
+    // ** click => 1 click
     // btn.addEventListener('click',eventHandler);
     // ul.addEventListener('click',eventHandler);
 
-    // dbclick
-    btn.addEventListener('dbclick',eventHandler)
+    //** double click
+    //btn.addEventListener('dblclick',eventHandler)
+
+    //** Mouse Down -> mouse on the right click -> keep
+    // btn.addEventListener('mousedown',eventHandler)
+
+    // //** Mouse Up -> mouse on the right click -> break up
+    // btn.addEventListener('mouseup',eventHandler)
+
+    // //** Mouse Enter -> Enter Container
+    // ul.addEventListener('mouseenter',eventHandler);
+
+    // //** Mouse Leave -> Leave Container
+    // ul.addEventListener('mouseleave',eventHandler)
+
+
+    // //** Mouse Over -> Include element belonging 
+    // ul.addEventListener('mouseover',eventHandler)
+
+    // //** Mouse Out -> Include element belonging 
+    // ul.addEventListener('mouseout',eventHandler)
 
     function eventHandler(event){
         console.log(`event type : ${event.type}`);
