26b60f2f6af7e660cead43bd273b446323ef27a2
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-10 13:00:11 +0400

    JavaScript > Event > Keyboard Event > add

--- 8_Events/JavaScripts/21_8_3_Keyboard_Event.js
+++ 8_Events/JavaScripts/21_8_3_Keyboard_Event.js
@@ -1,12 +1,37 @@
 // Keyboard Event
 const input = document.querySelector('#txtTaskName');
-    
+const form = document.querySelector("form")
+
     //** Keyboard Press
-    input.addEventListener('keydown',eventHandler)
+    //input.addEventListener('keydown',eventHandler)
 
     //** Keyboard Break up
-    input.addEventListener('keyup',eventHandler)
+    // input.addEventListener('keyup',eventHandler)
+
+    //** Keyboard Press
+    //input.addEventListener('keypress',eventHandler)
+
+    //** Focus
+    // input.addEventListener('focus',eventHandler)
+
+    //** Cut -> control + x -> press
+    // input.addEventListener('cut',eventHandler)
+
+    //** Paste -> control + v -> press
+    // input.addEventListener('paste',eventHandler)
+
+    //*-* Submit 
+    form.addEventListener('submit',eventHandler)
+
+    //*-* Select 
+    input.addEventListener('select',eventHandler)
+
+    
 
     function eventHandler(x){
-        console.log(`Event type : ${x.type}`);
+        console.log(`Event type : ${x.type} , Keyboard Code: ${x.keyCode} , Keyboard Value: ${x.target.value} `);
+
+
+        // x.preventDefault()
+        // x.target.style.backgroundColor='yellow'
     }
