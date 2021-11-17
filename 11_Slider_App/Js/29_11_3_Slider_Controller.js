ce43ef9c82d7eb8578e1ced1403007dc2e4a0fd9
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-17 18:42:43 +0400

    JavaScript > Slider App > Controller  > add

---
+++ 11_Slider_App/Js/29_11_3_Slider_Controller.js
@@ -0,0 +1,69 @@
+// 
+let data = [
+    {
+        name: 'Acher',
+        image: '../Img/bows.jfif',
+        link:'https://github.com/DrMadWill'
+    },
+    {
+        name: 'Nenula',
+        image: '../Img/Beautiful-Shooting-Star-4K-Wallpaper.jpg',
+        link:'https://github.com/DrMadWill'
+    },
+    {
+        name: 'Sky',
+        image: '../Img/navigate-using-stars-night-sky-1200x720.jpg',
+        link:'https://github.com/DrMadWill'
+    },
+    {
+        name: 'Panter',
+        image: '../Img/panter1.jfif',
+        link:'https://github.com/DrMadWill'
+    },
+    {
+        name: 'Sword',
+        image: '../Img/sword.jpg',
+        link:'https://github.com/DrMadWill'
+    },
+    {
+        name: 'Lin Ming',
+        image: '../Img/mizrak.jpg',
+        link:'https://github.com/DrMadWill'
+    }
+]
+
+const img = document.querySelector('#cardimg');
+const title = document.querySelector('.card-title');
+const link = document.querySelector('.card a');
+
+const rightContr = document.querySelector('#right')
+const leftContr = document.querySelector('#left')
+
+let val=3;
+let dataCount = data.length;
+showSilde(val)
+
+rightContr.addEventListener('click',function(w){
+    val++
+    if(val>=dataCount){
+        val=0;
+    }
+    showSilde(val);
+})
+
+leftContr.addEventListener('click',function(w){
+    val--
+    if(val<0){
+        val=dataCount-1
+    }
+    showSilde(val)
+})
+
+function showSilde(val){
+    img.setAttribute('src',data[val].image);
+    title.textContent = data[val].name;
+    link.setAttribute('src',data[val].link);
+}
+
+
+
