be3a34b2532141594df08d8b7f484c4ac8bfa057
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-12-07 14:15:20 +0400

    13 OOP > Object > Inheritance > Usage > add

--- 13_Object_Oriented_Programming/Js/43_13_1_Object.js
+++ 13_Object_Oriented_Programming/Js/43_13_1_Object.js
@@ -89,13 +89,56 @@ let mad = new Person("Mad",15,"Developer")
 let will = new Person("Will",21,"Wayer")
 
 
-console.log(mad)
-console.log(mad.lastName)
-console.log(mad.CaculateAge())
+// console.log(mad)
+// console.log(mad.lastName)
+// console.log(mad.CaculateAge())
 
-console.log("*********************")
+// console.log("*********************")
 
-console.log(will)
-console.log(will.lastName)
-console.log(will.GetName())
+// console.log(will)
+// console.log(will.lastName)
+// console.log(will.GetName())
 
+
+function Employe(name,salary){
+    if (!(this instanceof Employe)){ // cadition new keyword not use
+        return new Employe(name,salary)
+    }
+    this.name=name;
+    this.salary=salary;
+}
+
+Employe.prototype.caculateSalary=function(){
+    let month = new Date().getMonth()+1
+    let tax = 0 ;
+    var total = this.salary*month;
+
+    if (total<=2000){
+        tax=total*0.2;
+    }else if (total > 2000 && total <= 3000){
+        tax=total*0.25;
+    }else{
+        tax=total*0.3
+    }
+
+    return {
+        tax : tax,
+        pad : total - tax
+    }
+
+
+
+}
+
+var emp1= Employe("Samir",3000)
+console.log(emp1)
+console.log(emp1.caculateSalary())
+let salary1=emp1.caculateSalary()
+console.log("*************")
+
+var emp2=new Employe("Sadiq",4000)
+console.log(emp2)
+console.log(emp2.caculateSalary())
+
+
+console.log(` ${emp1.name} adlı şəxs tam ${salary1.tax} ilə vergi kəslimiş və qalan məbləğ ${salary1.pad} dir`)
