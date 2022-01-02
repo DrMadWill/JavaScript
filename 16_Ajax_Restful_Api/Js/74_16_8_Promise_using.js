1a15cdb008208f1b8d7e93a5bbc4690d8460b50f
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2022-01-02 23:44:01 +0400

    16 Ajax and Restful Api >  Promis > add

---
+++ 16_Ajax_Restful_Api/Js/74_16_8_Promise_using.js
@@ -0,0 +1,16 @@
+
+
+
+
+let promis = new Promise((resolve,reject)=>{
+    if(false){
+        resolve('sucess')
+    }else{
+        reject('Invalid')
+    }
+})
+
+// promis.then(e=>console.log(e)).catch(e=>console.log(e))
+
+ 
+
